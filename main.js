f8c6fb66ec89f2ae54acffb092c5027ddf97f6fe
zahrevans <526Zevans@frhsd.com>
2025-12-01 02:58:45 +0000

    list rendering

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             onSale: true,
             inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', },
+                { id: 2235, color: 'blue', }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 
