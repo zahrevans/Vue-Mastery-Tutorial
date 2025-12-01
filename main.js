9d7b9e91efacc1a45eeb0e22b5792877ccc9f4f9
zahrevans <526Zevans@frhsd.com>
2025-12-01 03:21:05 +0000

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'A pair of warm, fuzzy socks',
             image: './assets/images/socks_green.jpg',
@@ -8,11 +9,25 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', },
-                { id: 2235, color: 'blue', }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+
             ],
             sizes: ['S', 'M', 'L', 'XL'],
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        }
+        
     }
-
 })
