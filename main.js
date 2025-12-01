2e2188874f4d55002499ae401f5e2b1a32c770e4
zahrevans <526Zevans@frhsd.com>
2025-12-01 03:46:45 +0000

    Computed Properties

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'A pair of warm, fuzzy socks',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             onSale: true,
             inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
 
             ],
             sizes: ['S', 'M', 'L', 'XL'],
@@ -20,8 +21,8 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         },
         removeFromCart() {
             if (this.cart > 0) {
@@ -29,5 +30,16 @@ const app = Vue.createApp({
             }
         }
 
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
